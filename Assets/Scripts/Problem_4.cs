e97b3eedc61dd4162b0c5f4c5bdef142b2ea5a69
Dragzzz <priyashakala@gmail.com>
2022-08-10 19:59:22 -0700

    hw 3

---
+++ Assets/Scripts/Problem_4.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem_4 : MonoBehaviour
+{
+    /* Create a List of numbers that are odd and are divisible by the number 7 between 1 and 144.
+     * You should have a list that looks like this 7, 21, 35, 49, 63, 77, 91, 105, 119, 133
+     * Declare an array that's half the size of the list and save every other number from the list into the array 
+     * You should have a array that looks like this 7, 35, 63, 91, 119
+    */
+    void Start()
+    {
+        List<int> list = new List<int>();   
+        for(int i = 1; i < 144; i++)
+        {
+            if (i % 2 == 1 && i % 7 == 0)
+            {
+                list.Add(i);
+            }
+        }
+
+        int[] array = new int[list.Count / 2];
+
+        for(int i = 0; i < array.Length; i ++)
+        {
+            array[i] = ; list[i * 2];
+        }
+    }
+}
